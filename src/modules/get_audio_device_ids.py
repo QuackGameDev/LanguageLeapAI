7d2d79dcf4d9b8769e2d6eb34e9ceef53d393420
QuackGameDev <107073102+QuackGameDev@users.noreply.github.com>
2023-04-17 20:32:37 -0400

    Mouse Support for Game
    Device Id's now print in a file.
    Game in-game button as mouse button supported

--- src/modules/get_audio_device_ids.py
+++ src/modules/get_audio_device_ids.py
@@ -2,5 +2,8 @@ import speech_recognition as sr
 
 
 if __name__ == '__main__':
+    #Program changed to write it into a file instead. For people like me who for some reason have over 50 devices???
+    File_Thing = open("audios.txt","w")
     for mic_id, mic_name in enumerate(sr.Microphone.list_microphone_names()):
-        print(f'{mic_id}: {mic_name}')
+        File_Thing.write(f'{mic_id}: {mic_name}\n')
+    File_Thing.close()
